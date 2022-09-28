54cf500ef9506e5f92529380b3eacd9423e08d89
emreusun <emre95neo@gmail.com>
2022-09-28 15:16:24 -0400

    change

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// imports go at the top of the JS file
+import { profs } from "./modules/data.js";
+// this is an IIFE (Immediately Invoked Function Expression)
+// this is greate for encapsulating code / making it private
+// and is also a Javascript programming patter (the Module Pattern)
+
+(() => {
+    console.log('fired');
+    // get a reference to the template's contents and the target container
+    // into which we'll clone a copy of the markup
+    let theTemplate = document.querySelector("#user-template").content,
+       theTeam = document.querySelector('.team-section');
+
+    // let theHeader = document.querySelector(".user-name"),
+    //     theDesc = document.querySelector(".user-desc");
+
+    function changeCopy () {
+        // parse the top-level props from the profs object (the prof names)
+        let theProfs = Object.keys(profs);
+
+        // ['Trevor', 'John', 'Joe', 'Justin']
+
+        theProfs.forEach(prof => {
+        debugger;
+        // make a copy of the contents of the template tag
+        let panel = theTemplate.cloneNode(true),
+            containers = panel.firstElementChild.children; // the sections tag's contents
+        // put the prof data were it needs to go (just text for now)
+        containers[0].textContent = profs[prof].name;
+        containers[1].textContent = profs[prof].role;
+        
+        //paste the prof markup into the team section on the page
+        theTeam.appendChild(panel);
+    })
+}
+
+    changeCopy();
+})();
---
+++ js/modules/data.js
@@ -0,0 +1,33 @@
+const profs = {
+    Trevor: {
+        name: "Trevor", // string data type
+    role: "Prof", // string data type
+    nicknmae: "Olaf", // string data type
+    avatar: "trevor.jpg" // 
+    
+},
+John: {
+    name: "John", // string data type
+role: "Prof", // string data type
+nicknmae: "Morpheus", // string data type
+avatar: "john.jpg" // 
+
+},
+Joe: {
+    name: "Joe", // string data type
+role: "Prof", // string data type
+nicknmae: "pooh", // string data type
+avatar: "joe.jpg" // 
+
+},
+Trevor: {
+    name: "Justin", // string data type
+role: "Cordinator", // string data type
+nicknmae: "Carrel", // string data type
+avatar: "justin.jpg" // 
+
+},
+
+}
+
+export { profs }
